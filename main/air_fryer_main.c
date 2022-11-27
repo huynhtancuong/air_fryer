c7df1ab741f95fd4aa0df6b518ace55b43b0205c
huynhtancuong <huynhtancuong.hcm@gmail.com>
2022-11-27 21:07:32 +0300

    multiflexing ok

--- main/air_fryer_main.c
+++ main/air_fryer_main.c
@@ -16,6 +16,7 @@
 #include "input_touch_iot.h"
 #include "my_led.h"
 #include "output_io.h"
+#include "my_7_led.h"
 
 /* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
@@ -31,13 +32,21 @@
 #define BUTTON_POWER    TOUCH_PAD_NUM4
 
 // define led
-#define MINUS_LED_1 GPIO_NUM_25
-#define MINUS_LED_2 GPIO_NUM_26
-#define LED_4 GPIO_NUM_2
-#define LED_5 GPIO_NUM_15
-#define NUM_LED 16
+#define NUM_LED         8
+
+#define LED_7_A         GPIO_NUM_16
+#define LED_7_B         GPIO_NUM_2
+#define LED_7_C         GPIO_NUM_0
+#define LED_7_D         GPIO_NUM_4
+#define LED_7_E         GPIO_NUM_17
+#define LED_7_F         GPIO_NUM_5
+#define LED_7_G         GPIO_NUM_18
+#define LED_7_KAT1      GPIO_NUM_35
+#define LED_7_KAT2      GPIO_NUM_21
+#define LED_7_KAT3      GPIO_NUM_19
 
 my_led_t leds[NUM_LED];
+my_7_led_t leds_seg[3];
 
 void input_event_callback(int pin) {
     if(pin==GPIO_NUM_0) {
@@ -61,16 +70,21 @@ void led_matrix() {
     uint32_t multiplexing_speed = 10;
     while (1) {
         for (int i = 0; i< NUM_LED; i++) {
-            if (leds[i].is_on){
+            if ((leds[i].is_on) && (leds[i].minus_pin == GPIO_NUM_25)){
                 my_led_set_level(leds[i], LED_STATE_ON);
-                vTaskDelay(multiplexing_speed / portTICK_PERIOD_MS);
-                // my_led_set_level(leds[i], LED_STATE_OFF);
             }
-            else {
+        }
+        vTaskDelay(multiplexing_speed / portTICK_PERIOD_MS);
+        for (int i = 0; i< NUM_LED; i++) {
+            if ((leds[i].is_on) && (leds[i].minus_pin == GPIO_NUM_25)){
                 my_led_set_level(leds[i], LED_STATE_OFF);
             }
         }
     }
+}
+
+void led_7() {
+
 }
 
 void app_main(void)
@@ -83,38 +97,37 @@ void app_main(void)
 
     input_touch_set_callback(input_touch_event_callback);
     input_touch_init();
-    input_touch_io_create(BUTTON_PLUS, 20);
-    input_touch_io_create(BUTTON_MINUS, 20);
-    input_touch_io_create(BUTTON_MENU, 20);
-    input_touch_io_create(BUTTON_POWER, 10);
-    input_touch_io_create(BUTTON_TEMP, 10);
-    input_touch_io_create(BUTTON_TIME, 15);
+    // input_touch_io_create(BUTTON_PLUS, 20);
+    // input_touch_io_create(BUTTON_MINUS, 20);
+    // input_touch_io_create(BUTTON_MENU, 20);
+    // input_touch_io_create(BUTTON_POWER, 10);
+    // input_touch_io_create(BUTTON_TEMP, 10);
+    // input_touch_io_create(BUTTON_TIME, 15);
     
 
     
-    leds[0] = (my_led_t)    { .plus_pin = GPIO_NUM_0,   .minus_pin= GPIO_NUM_25, .is_on=1}; // LED 4
-    leds[1] = (my_led_t)    { .plus_pin = GPIO_NUM_4,   .minus_pin= GPIO_NUM_25, .is_on=1}; // LED 5
-    leds[2] = (my_led_t)    { .plus_pin = GPIO_NUM_2,   .minus_pin= GPIO_NUM_25, .is_on=1}; // LED 5
-    leds[3] = (my_led_t)    { .plus_pin = GPIO_NUM_16,  .minus_pin= GPIO_NUM_25, .is_on=1}; // LED 5
-    leds[4] = (my_led_t)    { .plus_pin = GPIO_NUM_17,  .minus_pin= GPIO_NUM_25, .is_on=1}; // LED 4
-    leds[5] = (my_led_t)    { .plus_pin = GPIO_NUM_5,   .minus_pin= GPIO_NUM_25, .is_on=1}; // LED 5
-    leds[6] = (my_led_t)    { .plus_pin = GPIO_NUM_15,  .minus_pin= GPIO_NUM_25, .is_on=1}; // LED 5
-    leds[7] = (my_led_t)    { .plus_pin = GPIO_NUM_18,  .minus_pin= GPIO_NUM_25, .is_on=1}; // LED 5
-
-    leds[8] = (my_led_t)    { .plus_pin = GPIO_NUM_0,   .minus_pin= GPIO_NUM_26, .is_on=1}; // LED 4
-    leds[9] = (my_led_t)    { .plus_pin = GPIO_NUM_4,   .minus_pin= GPIO_NUM_26, .is_on=1}; // LED 5
-    leds[10] = (my_led_t)   { .plus_pin = GPIO_NUM_2,   .minus_pin= GPIO_NUM_26, .is_on=1}; // LED 5
-    leds[11] = (my_led_t)   { .plus_pin = GPIO_NUM_16,  .minus_pin= GPIO_NUM_26, .is_on=1}; // LED 5
-    leds[12] = (my_led_t)   { .plus_pin = GPIO_NUM_17,  .minus_pin= GPIO_NUM_26, .is_on=1}; // LED 4
-    leds[13] = (my_led_t)   { .plus_pin = GPIO_NUM_5,   .minus_pin= GPIO_NUM_26, .is_on=1}; // LED 5
-    leds[14] = (my_led_t)   { .plus_pin = GPIO_NUM_15,  .minus_pin= GPIO_NUM_26, .is_on=1}; // LED 5
-    leds[15] = (my_led_t)   { .plus_pin = GPIO_NUM_18,  .minus_pin= GPIO_NUM_26, .is_on=1}; // LED 5
+    leds[0] = (my_led_t)    { .plus_pin = GPIO_NUM_0,   .minus_pin= NULL, .is_on=1}; // LED 4
+    leds[1] = (my_led_t)    { .plus_pin = GPIO_NUM_4,   .minus_pin= NULL, .is_on=1}; // LED 5
+    leds[2] = (my_led_t)    { .plus_pin = GPIO_NUM_2,   .minus_pin= NULL, .is_on=1}; // LED 5
+    leds[3] = (my_led_t)    { .plus_pin = GPIO_NUM_16,  .minus_pin= NULL, .is_on=1}; // LED 5
+    leds[4] = (my_led_t)    { .plus_pin = GPIO_NUM_17,  .minus_pin= NULL, .is_on=1}; // LED 4
+    leds[5] = (my_led_t)    { .plus_pin = GPIO_NUM_5,   .minus_pin= NULL, .is_on=0}; // LED 5
+    leds[6] = (my_led_t)    { .plus_pin = GPIO_NUM_15,  .minus_pin= NULL, .is_on=1}; // LED 5
+    leds[7] = (my_led_t)    { .plus_pin = GPIO_NUM_18,  .minus_pin= NULL, .is_on=1}; // LED 5
     
 
     for (int i = 0; i<NUM_LED; i++) {
         my_led_init(leds[i]);
     }
 
+
+    // leds_seg[0] = (my_7_led_t) { .A=LED_7_A, .B=LED_7_B, .C=LED_7_C, .D=LED_7_D, .E=LED_7_E, .F=LED_7_F, .G=LED_7_G, .Kat=LED_7_KAT1};
+    // leds_seg[1] = (my_7_led_t) { .A=LED_7_A, .B=LED_7_B, .C=LED_7_C, .D=LED_7_D, .E=LED_7_E, .F=LED_7_F, .G=LED_7_G, .Kat=LED_7_KAT2};
+    // leds_seg[2] = (my_7_led_t) { .A=LED_7_A, .B=LED_7_B, .C=LED_7_C, .D=LED_7_D, .E=LED_7_E, .F=LED_7_F, .G=LED_7_G, .Kat=LED_7_KAT3};
+
+    // my_7_led_set_state(leds_seg[0], 0b1111000);
+
     xTaskCreate(&led_matrix, "led_matrix", 2048, NULL, 5, NULL);
+    // xTaskCreate(&led_7, "led_7", 2048, NULL, 5, NULL);
     
 }
